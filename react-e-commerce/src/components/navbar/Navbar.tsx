b9632223e6e90cf85942406b7b7b8395799deee5
Phornhathai <phornhathai.aum@gmail.com>
2023-12-08 12:06:22 +0700

    update

--- react-e-commerce/src/components/navbar/Navbar.tsx
+++ react-e-commerce/src/components/navbar/Navbar.tsx
@@ -1,31 +1,38 @@
 import './navbar.css'
-import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Search ,ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from '@material-ui/core'
+
 
 const Navbar = () => {
-  return (
-    <div className='navbarContainer'>
-        <div className="navbarWrapper">
-            <div className="navbarLeft">
-                <div className="language">
-                    EN
+    return (
+        <div className='navbarContainer'>
+            <div className="navbarWrapper">
+                <div className="navbarLeft">
+                    <div className="language">
+                        EN
+                    </div>
+                    <div className="searchContainer">
+                        <input placeholder='Search' />
+                        <Search style={{ color: "gray", fontSize: 16 }} />
+                    </div>
                 </div>
-                <div className="searchContainer">
-                    <input placeholder='Search'/>
-                    <Search style={{ color:"gray" , fontSize : 16}} />
+                <div className="navbarCenter">
+                    <div className="navbarLogo">
+                        AUMMII DESU
+                    </div>
                 </div>
-            </div>
-            <div className="navbarCenter">
-                <div className="navbarLogo">
-                    AUMMII DESU
+                <div className="navbarRight">
+                    <div className="menuItem">REGISTER</div>
+                    <div className="menuItem">SIGN IN</div>
+                    <div className="menuItem">
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </div>
                 </div>
-            </div>
-            <div className="navbarRight">
-                <div className="menuItem">REGISTER</div>
-                <div className="menuItem">SIGN IN</div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
